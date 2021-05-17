a258eb918f1c7a9d1bdc2e48dd16e4ca7e3ac846
Janée Artis <terri.artis@du.edu>
2021-05-16 20:42:01 -0600

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Server-Side-APIs-Weather-Dashboard-HW-6
+
+## Table of Contents
+* Description
+* License
+* Contributors
+* Questions
+  
+  
+## Description
+This web application is a weather dashboard that features dynamically updated HTML and CSS. This web app uses OpenWeather API to retrieve weather data for cities and local storage to store persistent data.
+
+![_Users_janeeartis_Desktop_Bootcamp_Work_Server-Side-APIs-Weather-Dashboard-HW-6_index html](https://user-images.githubusercontent.com/78391244/118425300-22c2d980-b686-11eb-8438-3d455e8d22df.png)
+
+  
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+## Contributors
+Janée Artis
+  
+## Questions? Contact Me:
+* GitHub: [GitHub Profile](https://github.com/janeeart)
+* Email: janeeartis@gmail.com
+
--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,7 @@ function handleBtnSubmit() {
     console.log("works");
 var searchInputVal = this.textContent;
 console.log(searchInputVal);
-
+    
 GetWeatherData(searchInputVal)
 
 }
@@ -58,7 +58,8 @@ function GetWeatherData(value) {
 
 function submit (e) {
     e.preventDefault()
-
+    document.querySelector('#current-weather-row').setAttribute("class", "row")
+    document.querySelector('#five-day-forcast').setAttribute("class", "row")
     var value = document.querySelector(".form-control").value
     cityHistoryArr.push(value)
     if(cityHistoryArr.length>8){
@@ -67,68 +68,52 @@ function submit (e) {
     localStorage.setItem("history", JSON.stringify(cityHistoryArr))
     renderButtons()
     GetWeatherData(value)
-    // fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=c67ede96dd8b09ac55a1a15325cc40ad&units=imperial`)
-    //     .then(res => res.json())
-    //     .then(fiveDay => {
-    //         var filteredArr = fiveDay.list.filter(query => query.dt_txt.includes("12:00:00"))
-    //        // console.log(filteredArr);
-    //         printForcastCards(filteredArr);
-            
-    // })
-
-    // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=c67ede96dd8b09ac55a1a15325cc40ad&units=imperial`)
-    //     .then(res => res.json())
-    //     .then(currentDay => {
-    //         console.log(currentDay)
-            
-    //         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentDay.coord.lat}&lon=${currentDay.coord.lon}&appid=c67ede96dd8b09ac55a1a15325cc40ad`)
-    //             .then(res => res.json())
-    //             .then(({current}) => {
-    //                 printCurrentDay(currentDay, current.uvi)
-    //             })
-    //     })
-        // .then(function(currentDay) {
-        //     // printCurrentDay(currentDay)
-        // })
-
 }
 
 function printForcastCards(fiveDay){
     console.log(fiveDay);
-    var forcastTemp = $("#forecast-temp1");
-    forcastTemp.text(fiveDay[0].main.temp);
+    console.log(fiveDay[0].dt_txt.split(" ")[0])
+//     var dateOne = moment().format("(M/D/YYYY)");
+//     $("#forecast-date1").text(new Date().toLocaleDateString())
+//     var dayOneIcon = "http://openweathermap.org/img/w/" + fiveDay.weather[0].icon + ".png"
+//    $("#forecast-icon1").attr("src", dayOneIcon)
+// console.log(fiveDay.weather[0].icon)
+    var forecastTemp = $("#forecast-temp1");
+    forecastTemp.text(`Temp: ${fiveDay[0].main.temp}°F`);
     var forecastHumidity = $("#forecast-humidity1");
-    forecastHumidity.text(fiveDay[0].main.humidity);
-
-    var forcastTemp2 = $("#forecast-temp2");
-    forcastTemp2.text(fiveDay[1].main.temp);
+    forecastHumidity.text(`Humidity: ${fiveDay[0].main.humidity}`);
+    
+    var forecastTemp2 = $("#forecast-temp2");
+    forecastTemp2.text(`Temp: ${fiveDay[1].main.temp}°F`);
     var forecastHumidity2 = $("#forecast-humidity2");
-    forecastHumidity2.text(fiveDay[1].main.humidity);
+    forecastHumidity2.text(`Humidity: ${fiveDay[1].main.humidity}%`);
 
-    var forcastTemp3 = $("#forecast-temp3");
-    forcastTemp3.text(fiveDay[2].main.temp);
+    var forecastTemp3 = $("#forecast-temp3");
+    forecastTemp3.text(`Temp: ${fiveDay[2].main.temp}°F`);
     var forecastHumidity3 = $("#forecast-humidity3");
-    forecastHumidity3.text(fiveDay[2].main.humidity);
+    forecastHumidity3.text(`Humidity: ${fiveDay[2].main.humidity}%`);
 
-    var forcastTemp4 = $("#forecast-temp4");
-    forcastTemp4.text(fiveDay[3].main.temp);
+
+    var forecastTemp4 = $("#forecast-temp4");
+    forecastTemp4.text(`Temp: ${fiveDay[3].main.temp}°F`);
     var forecastHumidity4 = $("#forecast-humidity4");
-    forecastHumidity4.text(fiveDay[3].main.humidity);
+    forecastHumidity4.text(`Humidity: ${fiveDay[3].main.humidity}%`);
 
-    var forcastTemp5 = $("#forecast-temp5");
-    forcastTemp5.text(fiveDay[4].main.temp);
+
+    var forecastTemp5 = $("#forecast-temp5");
+    forecastTemp5.text(`Temp: ${fiveDay[4].main.temp}°F`);
     var forecastHumidity5 = $("#forecast-humidity5");
-    forecastHumidity5.text(fiveDay[4].main.humidity);
-
-
+    forecastHumidity5.text(`Humidity: ${fiveDay[4].main.humidity}%`);
 }
 
 function printCurrentDay(currentDay, uvi) {
     var cityName = $("#city-name");
-    console.log(currentDay.weather[0].icon)
-    var imgIcon = "http://openweathermap.org/img/wn/" + currentDay.weather[0].icon + "@2x.png"
-    console.log(imgIcon)
     cityName.text(currentDay.name);
+    var todayDate = moment().format("(M/D/YYYY)");
+    $("#current-date").text(new Date().toLocaleDateString())
+    console.log(currentDay.weather[0].icon)
+    var imgIcon = "http://openweathermap.org/img/w/" + currentDay.weather[0].icon + ".png"
+   $("#current-icon").attr("src", imgIcon)
     var currentTemp = $("#current-temp");
     currentTemp.text(`Temperature: ${currentDay.main.temp}°F`)
     var currentHumidity = $("#current-humidity")
